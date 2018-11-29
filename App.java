cecce021685c414b35c2d91febaf235d94ff794d
Sanjita <Sanjita.bhandary@tcs.com>
2018-11-29 16:22:50 +0530

    Java File

---
+++ App.java
@@ -0,0 +1,20 @@
+package com.sample.maven.testMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		int returnvalue = 0;
+
+		returnvalue = add(14, 79);
+
+		System.out.println("Hello World!" + returnvalue);
+
+	}
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+}
